f3e95f861f6a248ff40d958f3fbce84ffceb4f7c
Sébastien Huss <sebastien.huss@gmail.com>
2016-10-11 18:15:17 +0200

    Adding travis support

--- src/collectors.cpp
+++ src/collectors.cpp
@@ -284,6 +284,7 @@ CollectorsManager::CollectorsManager(HttpServer* p_server, Config* p_cfg) : serv
 	class stat st;
 	void *dlib;
 
+			std::cout << "Loading from " << directory << "\n";
 	dir = opendir(directory.c_str());
 	while ((ent = readdir(dir)) != NULL) {
 		const string file_name = ent->d_name;
@@ -296,6 +297,7 @@ CollectorsManager::CollectorsManager(HttpServer* p_server, Config* p_cfg) : serv
 			continue;
 
 		if (file_name.substr(file_name.rfind(".")) == ".so") {
+			std::cout << "Loading " << file_name << "\n";
 			dlib = dlopen(full_file_name.c_str(), RTLD_NOW);
 			if(dlib == NULL){
 				cerr << dlerror() << endl; 
