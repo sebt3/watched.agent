924c2a8ed0d6fca90091b96da596f435aa553975
Sébastien Huss <sebastien.huss@gmail.com>
2017-04-15 19:34:05 +0200

    Fixing travis #4

--- tools/travis/installdeps.sh
+++ tools/travis/installdeps.sh
@@ -2,12 +2,13 @@
 
 mkdir deps
 cd deps
-#git clone https://github.com/open-source-parsers/jsoncpp.git
-#mkdir jsoncpp/build
-#cd jsoncpp/build
-#cmake ..
-#make -j 4
-#sudo make install
+git clone https://github.com/open-source-parsers/jsoncpp.git
+mkdir jsoncpp/build
+cd jsoncpp/build
+cmake ..
+make -j 4
+sudo make install
+cd ../..
 git clone https://github.com/chriskohlhoff/asio.git
 cd asio/asio
 sed -i 's/SUBDIRS = include src/SUBDIRS = include/g' Makefile.am
