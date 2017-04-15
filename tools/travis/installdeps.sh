0a95e7c949788b21185b795cc7b5f8bb164dba3e
Sébastien Huss <sebastien.huss@gmail.com>
2017-04-15 19:24:13 +0200

    Updating travis build to use asio instead of boost

--- tools/travis/installdeps.sh
+++ tools/travis/installdeps.sh
@@ -2,9 +2,15 @@
 
 mkdir deps
 cd deps
-git clone https://github.com/open-source-parsers/jsoncpp.git
-mkdir jsoncpp/build
-cd jsoncpp/build
-cmake ..
-make -j 4
-sudo make install
+#git clone https://github.com/open-source-parsers/jsoncpp.git
+#mkdir jsoncpp/build
+#cd jsoncpp/build
+#cmake ..
+#make -j 4
+#sudo make install
+git clone https://github.com/chriskohlhoff/asio.git
+cd asio/asio
+sed -i 's/SUBDIRS = include src/SUBDIRS = include/g' Makefile.am
+sh autogen.sh
+./configure
+make install
